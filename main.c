7c4e49e27e3a25459f34e2cbb2a9691a75ed9744
Lizardo-Castillo <lcastillol@unsa.edu.pe>
2024-01-01 18:07:24 -0500

    Se añade la funciones 'BuildOneTwoThree' y para poder imprimir los valores 'printNode()' donde nos sale un error donde dice que no se puede comparar entre punteros y enteros

--- main.c
+++ main.c
@@ -1,11 +1,39 @@
-#include <stdib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 struct node{
 	int data;
 	struct node* next;
 };
 
-int main(){
+struct node* BuildOneTwoThree(){
+	struct node* head = malloc(sizeof(struct node));
+	struct node* second = malloc(sizeof(struct node));
+	struct node* third = malloc(sizeof(struct node));
 
-	return ;
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third;
+
+	third->data = 3;
+	third->next = NULL;
+
+	return head;
+}
+
+void printNode(struct node* example){
+	struct node* temp = malloc(sizeof(struct node));
+	temp = example;
+	while(temp->data != NULL){
+		printf("Data: %d\n", temp->data);
+		temp = temp->next;
+
+	}
+}
+int main(){
+	struct node* head = BuildOneTwoThree();
+	printNode(head);
+	return 0;
 }
